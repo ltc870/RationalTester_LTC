2d524d01661769c0cef0c4d5a5ace85a1dd33a93
Lawrence C <lawrencecaudle870@gmail.com>
2023-07-25 15:20:51 -0400

    removed extra spacing

--- src/Application.java
+++ src/Application.java
@@ -3,19 +3,19 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		 RationalNumber rationalNumber1 = new RationalNumber(10, 2);
-		 RationalNumber rationalNumber2 = new RationalNumber(10, 1);
-		 
-		 int answer = rationalNumber1.compareTo(rationalNumber2);
-		 
-		 if (answer == 0) {
-			 System.out.println("float1 is equal to float2");
-		 } else if (answer < 0) {
-			 System.out.println("float1 is less than float2");
-		 } else {
-			 System.out.println("float1 is greater than float2");
-		 }
+
+		RationalNumber rationalNumber1 = new RationalNumber(10, 2);
+		RationalNumber rationalNumber2 = new RationalNumber(10, 1);
+
+		int answer = rationalNumber1.compareTo(rationalNumber2);
+
+		if (answer == 0) {
+			System.out.println("float1 is equal to float2");
+		} else if (answer < 0) {
+			System.out.println("float1 is less than float2");
+		} else {
+			System.out.println("float1 is greater than float2");
+		}
 	}
 
 }
